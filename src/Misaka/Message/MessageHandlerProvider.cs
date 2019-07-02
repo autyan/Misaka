8ca8082b0cfa07e4160f0ce3cc4aa84befccfd00
Alex <alex.zhang@fortrun.cn>
2019-07-02 17:41:55 +0800

    Add MessgeHandlerProvider

--- src/Misaka/Message/MessageHandlerProvider.cs
+++ src/Misaka/Message/MessageHandlerProvider.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Misaka.Config;
 
 namespace Misaka.Message
 {
     public class MessageHandlerProvider
     {
+        private static readonly Dictionary<Type, Type[]> MessageHandlerTypes = new Dictionary<Type, Type[]>();
+
         public Type[] LookupHandlerTypes(Type messageType)
         {
-            throw new NotImplementedException();
+            if (!MessageHandlerTypes.ContainsKey(messageType))
+            {
+                LoadMessageHandlers(messageType);
+            }
+
+            return MessageHandlerTypes[messageType];
+        }
+
+        private void LoadMessageHandlers(Type messageType)
+        {
+            var types = TypeProvider.Instance.FindTypeInfos(info =>
+                                                            {
+                                                                return info.GetInterfaces()
+                                                                           .Any(i => i.IsGenericType
+                                                                                  && i.GetGenericTypeDefinition() ==
+                                                                                     typeof(IAsyncMessageSubscriber<>)
+                                                                                  && i.GetGenericArguments()[0] == messageType);
+                                                            });
+            var handlers = new List<Type>();
+            handlers.AddRange(types);
+            MessageHandlerTypes[messageType] = handlers.ToArray();
         }
     }
 }
--- src/Misaka/MessageQueue/PublishContext.cs
+++ src/Misaka/MessageQueue/PublishContext.cs
@@ -1,9 +1,13 @@
-﻿namespace Misaka.MessageQueue
+﻿using System;
+
+namespace Misaka.MessageQueue
 {
     public class PublishContext
     {
         public string Topic { get; set; }
 
         public object Message { get; set; }
+
+        public Exception PublishError { get; set; }
     }
 }
